5c15959ef787ddbc810b65fb5f61f9b75f039153
HenokTsegaye <maxhenock@gmail.com>
2021-03-15 23:37:53 +0300

    fix: cleaning up the typescript for the merge

--- components/audioPlayer/index.tsx
+++ components/audioPlayer/index.tsx
@@ -8,12 +8,18 @@ function AudioPlayer(): ReactElement {
     <div className="bg-gray-50 dark:bg-gray-900 shadow-sm h-full w-12/12 px-4 rounded-xl">
       <div className="flex flex-col justify-between h-3/4 w-full align-middle">
         <div className="grid col-2 w-full">
-          <h3 className="text-2xl text-center my-10 dark:text-gray-200 "> the title playing</h3>
+          <h3 className="text-2xl text-center my-10 dark:text-gray-200 ">
+            {' '}
+            the title playing
+          </h3>
           <img
             className="rounded-xl"
             src="https://zemachfm.com/wp-content/uploads/2021/02/wechat-tiktok-.png"
           />
-          <p className="text-center mt-5 dark:text-gray-200 "> the title playing</p>
+          <p className="text-center mt-5 dark:text-gray-200 ">
+            {' '}
+            the title playing
+          </p>
         </div>
 
         <div>
