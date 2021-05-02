e170f82d3ee210fd318b1e6a28cfe842fcd5c62c
HenokTsegaye <maxhenock@gmail.com>
2021-05-02 18:12:49 +0300

    feat: added dark support for story look

--- components/story/storyCard.tsx
+++ components/story/storyCard.tsx
@@ -3,9 +3,9 @@ import { storyProps } from './index.d';
 import Face from '../../icons/smiling-face-outline.svg';
 
 const StoryCard: FC<storyProps> = ({ title, description, color }) => (
-  <div className="bg-white p-4 text-left transition-all duration-100 hover:shadow-xl text-black rounded-lg">
+  <div className="bg-white dark:bg-gray-900 p-4 text-left transition-all duration-100 hover:shadow-xl text-black rounded-lg">
     <div
-      className={`text-3xl ${color} border-b border-red-50 pb-3 mb-4 text-left font-bold`}
+      className={`text-3xl ${color} border-b dark:border-gray-800 border-gray-50 pb-3 mb-4 text-left font-bold`}
     >
       {title}
       <span className=" float-right ">
@@ -13,7 +13,7 @@ const StoryCard: FC<storyProps> = ({ title, description, color }) => (
       </span>
     </div>
     <div
-      className="text-gray-500 text-sm"
+      className="text-gray-500 dark:text-gray-300 text-sm"
       dangerouslySetInnerHTML={{ __html: description }}
     ></div>
   </div>
