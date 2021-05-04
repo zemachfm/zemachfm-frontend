10e7a92134153e655cc9452a40b7008ca095b719
Abdulhamid <abdulhamidoumer3@gmail.com>
2021-05-04 06:30:07 +0300

    feature: cards integrated with api and loading state for host cards

--- lib/store/url.ts
+++ lib/store/url.ts
@@ -11,5 +11,5 @@ const CUSTOM_API_URL = `${BASE_URL}/wp-json/custom/v1`;
 const PODCASTS_URL = `${API_URL}/podcast`;
 const GUESTS_URL = `${PODCASTS_URL}?tags=82`;
 const SETTINGS_URL = `${CUSTOM_API_URL}/settings`;
-const HOSTS_URL = `${API_URL}/hosts`;
+const HOSTS_URL = `${CUSTOM_API_URL}/hosts`;
 export { BASE_URL, API_URL, PODCASTS_URL, SETTINGS_URL, GUESTS_URL, HOSTS_URL };
