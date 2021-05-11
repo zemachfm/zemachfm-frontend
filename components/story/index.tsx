3e9c51b74cbf06a4de05c6685cc4c1f18ca1b34e
HenokTsegaye <maxhenock@gmail.com>
2021-05-11 20:36:20 +0300

    feat: responsiveness fix for story.

--- components/story/index.tsx
+++ components/story/index.tsx
@@ -6,20 +6,25 @@ const OurStory: FC<storyIndex> = ({ story }) => {
   const { storyLine, cards, numberOfCards } = story;
   const { title, description } = storyLine;
   const colors = [
-    'text-green-500',
-    'text-indigo-500',
-    'text-yellow-500',
-    'text-red-500',
+    'bg-green-500',
+    'bg-indigo-500',
+    'bg-yellow-500',
+    'bg-red-500',
   ];
 
   return (
     <div id="our-story">
-      <h1 className=" text-6xl font-bold mt-8 mb-3 dark:text-white text-gray-900 "> {title} </h1>
+      <h1 className=" text-6xl font-bold mt-8 mb-3 dark:text-white text-gray-900 ">
+        {' '}
+        {title}{' '}
+      </h1>
       <div
         className="text-gray-400 mb-5 "
         dangerouslySetInnerHTML={{ __html: description }}
       ></div>
-      <div className={`grid gap-6 md:grid-cols-2 grid-cols-1 xl:grid-cols-${numberOfCards}`}>
+      <div
+        className={`grid gap-6 md:grid-cols-2 grid-cols-1 xl:grid-cols-${numberOfCards}`}
+      >
         {cards
           ? cards.map((card, index) =>
               card.title ? (
