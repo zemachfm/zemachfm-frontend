2f09cbcf96fe939f90f0aea44579c472521fe137
HenokTsegaye <maxhenock@gmail.com>
2021-05-13 16:28:04 +0300

    fix: fix with dark mode

--- components/story/storyCard.tsx
+++ components/story/storyCard.tsx
@@ -4,18 +4,16 @@ import Face from '../../icons/smiling-face-outline.svg';
 
 const StoryCard: FC<storyProps> = ({ title, description, color }) => (
   <div
-    className={`${color} dark:bg-gray-900 p-4 text-left transition-all duration-200 hover:shadow-xl text-black rounded-lg`}
+    className={`${color} p-4 text-left transition-all duration-200 hover:shadow-xl text-black rounded-lg`}
   >
-    <div
-      className={`text-3xl  dark:border-gray-800 text-white border-gray-50 pb-3 mb-4 text-left font-bold`}
-    >
+    <div className={`text-3xl  text-white pb-3 mb-4 text-left font-bold`}>
       {title}
       <span className=" float-right ">
         <Face className="fill-current rounded-full text-gray-200 w-12 h-12 p-2 " />
       </span>
     </div>
     <div
-      className="text-gray-100 dark:text-gray-300 text-sm"
+      className="text-gray-100 dark:text-gray-100 text-sm"
       dangerouslySetInnerHTML={{ __html: description }}
     ></div>
   </div>
