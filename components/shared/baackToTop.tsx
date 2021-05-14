5fef6d01c0151006f2a1bb2dc4f46e85fcf5c831
HenokTsegaye <maxhenock@gmail.com>
2021-05-14 23:59:15 +0300

    fix: fixed some issue with the backtotop that gets under the player some times

--- components/shared/baackToTop.tsx
+++ components/shared/baackToTop.tsx
@@ -1,9 +1,13 @@
 import React, { useEffect } from 'react';
+import { useSelector } from 'react-redux';
+import { IHomeReducer } from '../../store/home/types.d';
+import { TRootReducer } from '../../store/reducer';
 import ChecvronUpIcon from '../../icons/chevron-up.svg';
 
 const BackToTop = () => {
   const [showScroll, setShowScroll] = React.useState(false);
-
+  const state: IHomeReducer = useSelector((root: TRootReducer) => root.home);
+  const { currentPlay } = state.player;
   const checkScrollToTop = () => {
     if (!showScroll && window.pageYOffset > 400) {
       setShowScroll(true);
@@ -22,10 +26,12 @@ const BackToTop = () => {
 
   return showScroll ? (
     <button
-      className="fixed bottom-3 right-3 p-5 rounded-full bg-green-500 shadow-md z-10"
+      className={`fixed bottom-3 ${
+        currentPlay.item ? 'mb-40 lg:mb-28' : 'mb-10'
+      } lg:mr-8 mr-2 right-3 focus:outline-none outline-none lg:p-4 p-3 rounded-full dark:bg-gray-700 bg-white shadow-xl z-10`}
       onClick={scrollTop}
     >
-      <ChecvronUpIcon className="text-white" />
+      <ChecvronUpIcon className="text-black dark:text-white" />
     </button>
   ) : null;
 };
