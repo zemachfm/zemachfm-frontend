2ecbae9a485c10dfa4d5f43cd626afd94eba42c3
abdulhamidOumer <abdulhamidoumer3@gmail.com>
2021-05-25 23:35:24 +0300

    feat: just some radius on right side

--- components/rightSide/index.tsx
+++ components/rightSide/index.tsx
@@ -2,16 +2,16 @@ import { FC } from 'react';
 import rightSideProps from './types.d';
 
 const RightSidebar: FC<rightSideProps> = ({ content }) => (
-  <div className="flex flex-col rounded-2xl p-3 lg:ml-4 mt-2 overflow-hidden bg-white dark:bg-gray-900">
+  <div className="flex flex-col rounded-2xl lg:ml-4 mt-2 overflow-hidden bg-white dark:bg-gray-900">
     <div className="w-full h-auto">
       <img
         alt=""
-        className="rounded-2xl"
+        className="rounded-t-2xl"
         src={content.image.sizes['big-thumb']}
       />
     </div>
 
-    <div className="py-2 mt-5 border-gray-200 ">
+    <div className="p-3 mt-5 border-gray-200 ">
       <p className="mb-4 dark:text-gray-100 text-xl ">{content.title}</p>
       <div
         className="text-gray-500 dark:text-gray-400 mb-6"
