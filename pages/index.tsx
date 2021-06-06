de87b73249876ed37324ec4f52a51c256b7abd1a
abdulhamidOumer <abdulhamidoumer3@gmail.com>
2021-06-06 19:48:44 +0300

    feature: ready for deployment

--- pages/index.tsx
+++ pages/index.tsx
@@ -224,7 +224,7 @@ export const getStaticProps = wrapper.getStaticProps(
   }: GetStaticPropsContext & {
     store: any;
   }) => {
-    // await MakeRSS();
+    await MakeRSS();
     store.dispatch(fetchEpisodes());
     store.dispatch(fetchSettings(locale));
     store.dispatch(fetchGuests());
