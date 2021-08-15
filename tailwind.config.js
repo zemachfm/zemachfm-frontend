b2ee7d04641f573d192beb5119e7b25690715767
HenokTsegaye <maxhenock@gmail.com>
2021-08-15 19:26:20 +0300

    fix: fixing some configuration files for a test

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@
 
 module.exports = {
   // Removes unused css in production
-  purge: ['./pages/**/*.tsx', './components/**/*.tsx'],
+  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: 'class', // or 'media' or 'class'
   theme: {
     extend: {
@@ -22,16 +22,16 @@ module.exports = {
       },
     },
     screens: {
-      'sm': '640px',
+      sm: '640px',
       // => @media (min-width: 640px) { ... }
 
-      'md': '768px',
+      md: '768px',
       // => @media (min-width: 768px) { ... }
 
-      'lg': '1024px',
+      lg: '1024px',
       // => @media (min-width: 1024px) { ... }
 
-      'xl': '1280px',
+      xl: '1280px',
       // => @media (min-width: 1280px) { ... }
 
       '2xl': '1600px',
